0124e6de227ca42def1296a80050afe24119abb8
DvaMishkiLapa <artem132rus@gmail.com>
2019-02-12 16:28:57 +0300

    Added echo-mode

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_terminal.h"
+#include <typeinfo>
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -9,4 +11,70 @@ MainWindow::MainWindow(QWidget *parent) :
 
 MainWindow::~MainWindow(){
     delete ui;
+}
+
+// tank control
+void MainWindow::on_tank_forward_clicked(){
+    std::cout << "### tank_forward_clicked ###\n";
+}
+
+void MainWindow::on_tank_back_clicked(){
+    std::cout << "### tank_back_clicked ###\n";
+}
+
+void MainWindow::on_tank_left_clicked(){
+    std::cout << "### tank_left_clicked ###\n";
+}
+
+void MainWindow::on_tank_right_clicked(){
+    std::cout << "### tank_right_clicked ###\n";
+}
+
+void MainWindow::on_tank_stop_clicked(){
+    std::cout << "### tank_stop_clicked ###\n";
+}
+
+// tower control
+void MainWindow::on_gun_up_clicked(){
+    std::cout << "### gun_up_clicked ###\n";
+}
+
+void MainWindow::on_gun_down_clicked(){
+    std::cout << "### gun_down_clicked ###\n";
+}
+
+void MainWindow::on_gun_left_clicked(){
+    std::cout << "### gun_left_clicked ###\n";
+}
+
+void MainWindow::on_gun_right_clicked(){
+    std::cout << "### tank_right_clicked ###\n";
+}
+
+void MainWindow::on_gun_fire_clicked(){
+    std::cout << "### gun_fire_clicked ###\n";
+}
+
+// security activation
+void MainWindow::on_protection_clicked(){
+    std::cout << "### gun_fire_clicked ###\n";
+}
+
+// weapon control
+void MainWindow::on_type_gun_comboBox_currentIndexChanged(){
+    std::cout << "### currentIndexChanged type_gun_comboBox <";
+    std::cout << ui->type_gun_comboBox->currentText().toUtf8().data();
+    std::cout << "> ###\n";
+}
+
+void MainWindow::on_type_ammo_comboBox_currentIndexChanged(){
+    std::cout << "### currentIndexChanged type_ammo_comboBox <";
+    std::cout << ui->type_ammo_comboBox->currentText().toUtf8().data();
+    std::cout << "> ###\n";
+}
+
+void MainWindow::on_type_shoot_comboBox_currentIndexChanged(){
+    std::cout << "### currentIndexChanged type_shoot_comboBox <";
+    std::cout << ui->type_shoot_comboBox->currentText().toUtf8().data();
+    std::cout << "> ###\n";
 }
--- mainwindow.h
+++ mainwindow.h
@@ -14,6 +14,29 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    // tank control slots
+    void on_tank_forward_clicked();
+    void on_tank_back_clicked();
+    void on_tank_left_clicked();
+    void on_tank_right_clicked();
+    void on_tank_stop_clicked();
+
+    // tower control slots
+    void on_gun_up_clicked();
+    void on_gun_down_clicked();
+    void on_gun_left_clicked();
+    void on_gun_right_clicked();
+    void on_gun_fire_clicked();
+
+    // security activation slots
+    void on_protection_clicked();
+
+    // weapon control
+    void on_type_gun_comboBox_currentIndexChanged();
+    void on_type_ammo_comboBox_currentIndexChanged();
+    void on_type_shoot_comboBox_currentIndexChanged();
+
 private:
     Ui::MainWindow *ui;
 };
