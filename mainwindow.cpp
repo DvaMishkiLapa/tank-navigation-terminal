5789a5725e3d9cb81c4678b0603d509225c8b1f4
DvaMishkiLapa <artem132rus@gmail.com>
2019-03-02 00:20:05 +0300

    Added full remote control support

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,80 +1,266 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+// #include <cstdio>
+// #include <unistd.h>
+// #include <stdlib.h>
+// #include <string>
+// #include <vector>
+#include <arpa/inet.h>
+#include <unistd.h>
+
 #include "mainwindow.h"
 #include "ui_terminal.h"
-#include <typeinfo>
-#include <iostream>
+
+#include "command.pb.h"
+
+
+using namespace std;
+
+RecvThreads::RecvThreads(){
+}
+
+RecvThreads::~RecvThreads(){
+}
+
+
+bool MainWindow::void_slot(){
+    emit void_signal();
+    return true;
+}
+
+
+void RecvThreads::run(){
+    int sock, listener;
+    struct sockaddr_in addr;
+    listener = socket(AF_INET, SOCK_STREAM, 0);
+    if(listener < 0){
+        perror("socket");
+        exit(1);
+    }
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1488);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0){
+        perror("bind");
+        exit(2);
+    }
+    listen(listener, 1);
+    while(true){
+        sock = accept(listener, NULL, NULL);
+        if(sock < 0){
+            perror("accept");
+            exit(3);
+        }
+        while(true){
+            usleep(16667);
+            std::vector<char> buffer(1024);
+            std::string rcv;
+            int bytesReceived = 0;
+            do {
+                bytesReceived = recv(sock, &buffer[0], buffer.size(), 0);
+                if ( bytesReceived == -1 ) { 
+                    cout << "bytesReceived == -1" << endl;
+                } else {
+                    rcv.append( buffer.cbegin(), buffer.cend());
+                }
+            } while ( bytesReceived == 1024 );
+
+            protocol::Data data;
+            data.ParseFromString(rcv);
+            QString str_speed = QString::number(data.speed());
+            QString str_distance = QString::number(data.distance());
+            QString str_ammo = QString::number(data.ammo());
+            QString str_gun_angle = QString::number(data.gun_angle());
+            QString str_protect = QString::number(data.protect());
+            QString str_machine_turn = QString::number(data.machine_turn());
+            QString str_gun_turn = QString::number(data.gun_turn());
+
+            protocol::Data::coords location;
+            location.ParseFromString(rcv);
+            QString str_x = QString::number(location.x());
+            QString str_y = QString::number(location.y());
+            emit processDone(str_speed, str_distance, str_ammo, str_gun_angle, str_protect, str_machine_turn, str_gun_turn, str_x, str_y);
+        }
+        ::close(sock);
+    }
+}
+
+
+void MainWindow::act_send(string &act_str){
+    protocol::Action act;
+    act.set_action(act_str);
+    string buffer;
+    act.SerializeToString(&buffer);
+    send(sock, buffer.c_str(), buffer.size(), 0);
+    act.ParseFromString(buffer);
+}
+
+
+void MainWindow::start_connection(){
+    struct sockaddr_in addr;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0){
+        perror("socket");
+        exit(1);
+    }
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr("192.168.1.214");
+    // addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    addr.sin_port = htons(1488); // или любой другой порт...
+    if(::connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0){
+        perror("connect");
+        exit(2);
+    }
+}
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow){
     ui->setupUi(this);
+    start_connection();
+
+    bool bConnection = connect(m_pqThread, SIGNAL(void_signal()), this, SLOT(startThread(bool)));
+    m_pqThread = new RecvThreads();
+    bConnection = connect(m_pqThread,
+                          SIGNAL(processDone(QString, QString, QString, QString, QString, QString, QString, QString, QString)),
+                          this,
+                          SLOT(threadProcessDone(QString, QString, QString, QString, QString, QString, QString, QString, QString)));
+    void_signal();
 }
 
+
 MainWindow::~MainWindow(){
+    delete m_pqThread;
     delete ui;
+}
+
+
+void MainWindow::startThread(bool){
+	m_pqThread->start();
+}
+
+
+void MainWindow::threadProcessDone(QString str_speed,
+                                   QString str_distance,
+                                   QString str_ammo,
+                                   QString str_gun_angle,
+                                   QString str_protect,
+                                   QString str_machine_turn,
+                                   QString str_gun_turn,
+                                   QString str_x,
+                                   QString str_y){
+	ui->speed_label->setText(str_speed);
+    ui->distance_label->setText(str_distance);
+    ui->ammo_label->setText(str_ammo);
+    ui->gun_angle_label->setText(str_gun_angle);
+    // ui->protect_label->setText(str_protect);
+    ui->turn_label->setText(str_machine_turn);
+    ui->gun_turn_label->setText(str_gun_turn);
+    QString coord_str = str_x + ", " + str_y;
+    ui->coord_label->setText(str_x);
 }
 
 // tank control
 void MainWindow::on_tank_forward_clicked(){
-    std::cout << "### tank_forward_clicked ###\n";
+    cout << "### tank_forward_clicked ###\n";
+    string act = "tank_forward";
+    act_send(act);
 }
+
 
 void MainWindow::on_tank_back_clicked(){
-    std::cout << "### tank_back_clicked ###\n";
+    cout << "### tank_back_clicked ###\n";
+    string act = "tank_back";
+    act_send(act);
 }
+
 
 void MainWindow::on_tank_left_clicked(){
-    std::cout << "### tank_left_clicked ###\n";
+    cout << "### tank_left_clicked ###\n";
+    string act = "tank_left";
+    act_send(act);
+    
 }
+
 
 void MainWindow::on_tank_right_clicked(){
-    std::cout << "### tank_right_clicked ###\n";
+    cout << "### tank_right_clicked ###\n";
+    string act = "tank_right";
+    act_send(act);
 }
 
+
 void MainWindow::on_tank_stop_clicked(){
-    std::cout << "### tank_stop_clicked ###\n";
+    cout << "### tank_stop_clicked ###\n";
+    string act = "tank_stop";
+    act_send(act);
 }
 
 // tower control
 void MainWindow::on_gun_up_clicked(){
-    std::cout << "### gun_up_clicked ###\n";
+    cout << "### gun_up_clicked ###\n";
+    string act = "gun_up";
+    act_send(act);
 }
+
 
 void MainWindow::on_gun_down_clicked(){
-    std::cout << "### gun_down_clicked ###\n";
+    cout << "### gun_down_clicked ###\n";
+    string act = "gun_down";
+    act_send(act);
 }
+
 
 void MainWindow::on_gun_left_clicked(){
-    std::cout << "### gun_left_clicked ###\n";
+    cout << "### gun_left_clicked ###\n";
+    string act = "gun_left";
+    act_send(act);
 }
+
 
 void MainWindow::on_gun_right_clicked(){
-    std::cout << "### tank_right_clicked ###\n";
+    cout << "### tank_right_clicked ###\n";
+    string act = "gun_right";
+    act_send(act);
 }
 
+
 void MainWindow::on_gun_fire_clicked(){
-    std::cout << "### gun_fire_clicked ###\n";
+    cout << "### gun_fire_clicked ###\n";
+    string act = "gun_fire";
+    act_send(act);
 }
 
 // security activation
 void MainWindow::on_protection_clicked(){
-    std::cout << "### gun_fire_clicked ###\n";
+    cout << "### protection_clicked ###\n";
+    string act = "protect";
+    act_send(act);
+    // ::close(sock);
 }
 
 // weapon control
 void MainWindow::on_type_gun_comboBox_currentIndexChanged(){
-    std::cout << "### currentIndexChanged type_gun_comboBox <";
-    std::cout << ui->type_gun_comboBox->currentText().toUtf8().data();
-    std::cout << "> ###\n";
+    cout << "### currentIndexChanged type_gun_comboBox <";
+    cout << ui->type_gun_comboBox->currentText().toUtf8().data();
+    cout << "> ###\n";
 }
+
 
 void MainWindow::on_type_ammo_comboBox_currentIndexChanged(){
-    std::cout << "### currentIndexChanged type_ammo_comboBox <";
-    std::cout << ui->type_ammo_comboBox->currentText().toUtf8().data();
-    std::cout << "> ###\n";
+    cout << "### currentIndexChanged type_ammo_comboBox <";
+    cout << ui->type_ammo_comboBox->currentText().toUtf8().data();
+    cout << "> ###\n";
 }
 
+
 void MainWindow::on_type_shoot_comboBox_currentIndexChanged(){
-    std::cout << "### currentIndexChanged type_shoot_comboBox <";
-    std::cout << ui->type_shoot_comboBox->currentText().toUtf8().data();
-    std::cout << "> ###\n";
+    cout << "### currentIndexChanged type_shoot_comboBox <";
+    cout << ui->type_shoot_comboBox->currentText().toUtf8().data();
+    cout << "> ###\n";
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,24 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+// #include <string>
+#include <QThread>
 #include <QMainWindow>
+
+
+class RecvThreads :
+    public QThread
+{
+    Q_OBJECT
+
+public:
+    RecvThreads();
+    ~RecvThreads();
+    void run();
+
+signals:
+    void processDone(QString, QString, QString, QString, QString, QString, QString, QString, QString);					//SIGNAL to show that some process have been done
+};
+
 
 namespace Ui {
 class MainWindow;
@@ -10,35 +27,45 @@ class MainWindow;
 class MainWindow : public QMainWindow{
     Q_OBJECT
 
-public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    public:
+        explicit MainWindow(QWidget *parent = 0);
+        Ui::MainWindow *ui;
+        RecvThreads* m_pqThread;
+        ~MainWindow();
+        int sock = 0;
 
-private slots:
-    // tank control slots
-    void on_tank_forward_clicked();
-    void on_tank_back_clicked();
-    void on_tank_left_clicked();
-    void on_tank_right_clicked();
-    void on_tank_stop_clicked();
+    signals:
+        bool void_signal();
 
-    // tower control slots
-    void on_gun_up_clicked();
-    void on_gun_down_clicked();
-    void on_gun_left_clicked();
-    void on_gun_right_clicked();
-    void on_gun_fire_clicked();
+    public slots:
+        bool void_slot();
+    	void startThread(bool);
+	    void threadProcessDone(QString, QString, QString, QString, QString, QString, QString, QString, QString);
 
-    // security activation slots
-    void on_protection_clicked();
+        void act_send(std::string &act);
+        void start_connection();
 
-    // weapon control
-    void on_type_gun_comboBox_currentIndexChanged();
-    void on_type_ammo_comboBox_currentIndexChanged();
-    void on_type_shoot_comboBox_currentIndexChanged();
+        // tank control slots
+        void on_tank_forward_clicked();
+        void on_tank_back_clicked();
+        void on_tank_left_clicked();
+        void on_tank_right_clicked();
+        void on_tank_stop_clicked();
 
-private:
-    Ui::MainWindow *ui;
+        // tower control slots
+        void on_gun_up_clicked();
+        void on_gun_down_clicked();
+        void on_gun_left_clicked();
+        void on_gun_right_clicked();
+        void on_gun_fire_clicked();
+
+        // security activation slots
+        void on_protection_clicked();
+
+        // weapon control
+        void on_type_gun_comboBox_currentIndexChanged();
+        void on_type_ammo_comboBox_currentIndexChanged();
+        void on_type_shoot_comboBox_currentIndexChanged();
 };
 
 #endif // MAINWINDOW_H
